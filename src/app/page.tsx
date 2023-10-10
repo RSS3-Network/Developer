1aa937074fb850c64182c4a671bc5316ae17c614
DIYgod <i@diygod.me>
2023-10-11 02:00:44 +0800

    feat: sticky nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ConnectButton from "@/components/ConnectButton";
 import { FirstScreenBg } from "@/components/first-screen";
 import { DESCRIPTION } from "@/lib/env";
+import { StickyNav } from "@/components/sticky-nav";
 
 export default function Home() {
   return (
     <div className="h-full flex items-center justify-center flex-col space-y-8">
+      <StickyNav theme="light" />
       <FirstScreenBg />
       <h1 className="text-8xl font-light text-center space-y-8 px-8">
         {DESCRIPTION}
