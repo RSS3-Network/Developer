b9aecaabadc6d7898185c993ef9c128e9f08a782
Nya Candy <dev@candinya.com>
2023-10-16 12:21:32 +0800

    feat: gateway api reverse proxy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/gateway/:path*",
+        destination: `https://gateway.${
+          process.env.NODE_ENV === "production" ? "" : "dev."
+        }rss3.io/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
