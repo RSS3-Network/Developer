340b6250f0577764bcbe28a25368a4dd4b1bab59
Songkeys <i@song.work>
2024-06-04 00:26:21 +0800

    feat: update api reference openapi souce

--- src/app/reference/route.ts
+++ src/app/reference/route.ts
@@ -1,6 +1,6 @@
 import { ApiReference } from "@scalar/nextjs-api-reference"
 
-const content = await fetch("https://gi.rss3.io/docs/openapi.yaml").then((r) =>
+const content = await fetch("https://gi.rss3.io/docs/openapi.json").then((r) =>
 	r.text(),
 )
 
