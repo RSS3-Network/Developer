8d3fb64178b467fdf6937daab7cabd8031d33a4e
Songkeys <i@song.work>
2024-06-06 17:38:33 +0800

    fix(reference): use url for real-time rendering

--- src/app/reference/route.ts
+++ src/app/reference/route.ts
@@ -1,11 +1,12 @@
 import { ApiReference } from "@scalar/nextjs-api-reference"
 
-const content = await fetch("https://gi.rss3.io/docs/openapi.json").then((r) =>
-	r.text(),
-)
+// const content = await fetch("https://gi.rss3.io/docs/openapi.json").then((r) =>
+// 	r.text(),
+// )
 
 export const GET = ApiReference({
 	spec: {
-		content,
+		url: "https://gi.rss3.io/docs/openapi.json",
+		// content,
 	},
 })
